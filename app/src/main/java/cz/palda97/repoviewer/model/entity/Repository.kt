9a1eea3b645535cd64e4638048e975e60d07a4c6
David Paleček <palecda1@fit.cvut.cz>
2021-10-08 17:42:00 +0200

    Add repository data class for github user repository representation

---
+++ app/src/main/java/cz/palda97/repoviewer/model/entity/Repository.kt
@@ -0,0 +1,11 @@
+package cz.palda97.repoviewer.model.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Repository(
+    @PrimaryKey(autoGenerate = false) val id: Long,
+    val name: String,
+    val description: String?
+)
