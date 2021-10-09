6ebcb201e5eea0793947e5080139b74a9be0f079
David Paleček <palecda1@fit.cvut.cz>
2021-10-09 04:05:16 +0200

    Link main activity buttons to the viewmodel and add progress bar

--- app/src/main/java/cz/palda97/repoviewer/view/MainActivity.kt
+++ app/src/main/java/cz/palda97/repoviewer/view/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import cz.palda97.repoviewer.R
 import cz.palda97.repoviewer.databinding.ActivityMainBinding
+import cz.palda97.repoviewer.viewmodel.MainScreenViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +13,29 @@ class MainActivity : AppCompatActivity() {
     private val binding
         get() = _binding!!
 
+    private val viewModel by lazy {
+        MainScreenViewModel.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        setupShowRepositoriesButton()
+        setupAboutAppButton()
+    }
+
+    private fun setupShowRepositoriesButton() {
+        binding.showRepositoriesButton.setOnClickListener {
+            viewModel.showRepositoriesButton(binding.nameField.text.toString())
+        }
+    }
+
+    private fun setupAboutAppButton() {
+        binding.aboutAppButton.setOnClickListener {
+            viewModel.aboutAppButton()
+        }
     }
 
     override fun onDestroy() {
