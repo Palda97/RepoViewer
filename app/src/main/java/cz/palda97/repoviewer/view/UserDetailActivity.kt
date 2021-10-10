af458a91df5758be717f17c67442d1f619767be3
David Paleček <palecda1@fit.cvut.cz>
2021-10-10 21:54:31 +0200

    Add dividers to recycler view in user detail screen

--- app/src/main/java/cz/palda97/repoviewer/view/UserDetailActivity.kt
+++ app/src/main/java/cz/palda97/repoviewer/view/UserDetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import cz.palda97.repoviewer.R
 import cz.palda97.repoviewer.databinding.ActivityUserDetailBinding
+import cz.palda97.repoviewer.view.RecyclerViewCosmetics.addDividers
 import cz.palda97.repoviewer.viewmodel.UserDetailViewModel
 
 class UserDetailActivity : AppCompatActivity() {
@@ -26,6 +27,7 @@ class UserDetailActivity : AppCompatActivity() {
     private fun setupRecyclerView() {
         val adapter = RepositoryRecyclerAdapter { TODO() }
         binding.repoRecyclerview.adapter = adapter
+        binding.repoRecyclerview.addDividers(this)
         viewModel.liveRepositories.observe(this, {
             val repositories = it ?: return@observe
             adapter.updateRepositoryList(repositories)
