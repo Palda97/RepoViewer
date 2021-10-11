b926768c25edb62af19dc49675a220c685339afc
David Paleček <palecda1@fit.cvut.cz>
2021-10-11 14:20:56 +0200

    Move RepoRepository.StatusCode.msg from fragments to viewmodel

--- app/src/main/java/cz/palda97/repoviewer/view/repodetail/RecentCommitsFragment.kt
+++ app/src/main/java/cz/palda97/repoviewer/view/repodetail/RecentCommitsFragment.kt
@@ -8,9 +8,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import cz.palda97.repoviewer.R
 import cz.palda97.repoviewer.databinding.FragmentRecentCommitsBinding
-import cz.palda97.repoviewer.model.repository.RepoRepository
 import cz.palda97.repoviewer.view.RecyclerViewCosmetics.addDividers
 import cz.palda97.repoviewer.viewmodel.repodetail.RepoDetailViewModel
+import cz.palda97.repoviewer.viewmodel.repodetail.RepoDetailViewModel.Companion.msg
 
 class RecentCommitsFragment : Fragment() {
 
@@ -42,15 +42,6 @@ class RecentCommitsFragment : Fragment() {
             adapter.updateCommitList(commits)
         })
     }
-
-    private val RepoRepository.StatusCode.msg
-        get() = when (this) {
-            RepoRepository.StatusCode.NOT_FOUND -> getString(R.string.user_has_not_been_found)
-            RepoRepository.StatusCode.CONNECTION_PROBLEM -> getString(R.string.there_is_a_connection_problem)
-            RepoRepository.StatusCode.PARSING_ERROR -> getString(R.string.there_is_a_parsing_problem)
-            RepoRepository.StatusCode.LOADING -> getString(R.string.internal_error)
-            RepoRepository.StatusCode.OK -> getString(R.string.internal_error)
-        }
 
     private fun setupStatusCode() {
         viewModel.liveCommitStatus.observe(viewLifecycleOwner, {
